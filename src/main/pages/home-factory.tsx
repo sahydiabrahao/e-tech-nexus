9b2555f0ee240420c8ec1d4b858cee7d509e5463
sahydiabrahao <sahydi@protonmail.com>
2023-04-18 07:26:24 -0400

    feat: start e-tech nexus project

---
+++ src/main/pages/home-factory.tsx
@@ -0,0 +1,11 @@
+
+import Hero from '@/presentation/pages/hero/hero'
+import React from 'react'
+
+export const MakeHome: React.FC = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
---
+++ src/presentation/pages/hero/hero.tsx
@@ -0,0 +1,14 @@
+import Styles from './hero-styles.scss'
+import React from 'react'
+
+const Hero: React.FC = () => {
+  return (
+    <>
+      <div className={Styles.hero}>
+        <h1>oi</h1>
+      </div>
+    </>
+  )
+}
+
+export default Hero
